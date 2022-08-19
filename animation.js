1661650f4079ef064576f54100d1ad24efcf23f6
Ivan Samusevych <samusevych.ivan@gmail.com>
2022-08-19 14:43:54 +0300

    Adds registration form (not finished)

--- animation.js
+++
@@ -1,5 +0,0 @@
-$(".fade-in-first").addClass("animated fadeInUp");
-setTimeout(function(){
-     $(".fade-in-second").animate({ opacity: 1 });
-    $(".btn-explore").addClass("pulse-anim");
-}, 1300);
